41cf322e01c4132de66f537f322b18a63b2e3c41
edgetechacademy <gjames@ancoraeducation.com>
2018-04-09 07:38:00 -0500

    Create a random value for the type field

--- src/main/java/edu/edgetech/sb2/bootstrap/ProductLoader.java
+++ src/main/java/edu/edgetech/sb2/bootstrap/ProductLoader.java
@@ -1,8 +1,6 @@
 package edu.edgetech.sb2.bootstrap;
 
-import edu.edgetech.sb2.domain.Customer;
 import edu.edgetech.sb2.domain.Product;
-import edu.edgetech.sb2.services.CustomerService;
 import edu.edgetech.sb2.services.ProductService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -16,46 +14,31 @@ import java.math.BigDecimal;
 @Component
 public class ProductLoader implements ApplicationListener<ContextRefreshedEvent> {
 	@Autowired
-    private ProductService productService;
-
-	@Autowired
-	private CustomerService customerService;
+	private ProductService productService;
 
 	private static final Logger log = LogManager.getLogger(ProductLoader.class);
 
-    @Override
-    public void onApplicationEvent(ContextRefreshedEvent event) {
+	@Override
+	public void onApplicationEvent(ContextRefreshedEvent event) {
 
-		createProduct("112358132134", 			"Edge Tech Agile", 			"/images/agile.png"		);
-		createProduct("6.022140857×10^23",		"Edge Tech C#", 			"/images/csharp.png"	);
-		createProduct("8.314459848",			"Edge Tech CSS", 			"/images/css.png"		);
-		createProduct("1.6021766×10^−19",	 	"Edge Tech HTML5", 			"/images/html.png" 		);
-		createProduct("137.036", 				"Edge Tech Java 1.8", 		"/images/java.png"		);
-		createProduct("9.46073047 x 10^12", 	"Edge Tech JavaScript", 	"/images/javascript.png");
-		createProduct("2.718281828459045",		"Edge Tech MongoDB", 		"/images/mongodb.png"	);
-		createProduct("299792458", 				"Edge Tech MySQL",		 	"/images/mysql.png"		);
-		createProduct("13.799±0.021*10^9", 		"Edge Tech Spring Boot",	"/images/spring.png"	);
-		createProduct("384400", 				"Edge Tech SQL", 			"/images/sql.png"		);
+		createProduct("112358132134", "Edge Tech Agile", "/images/agile.png");
+		createProduct("6.022140857×10^23", "Edge Tech C#", "/images/csharp.png");
+		createProduct("8.314459848", "Edge Tech CSS", "/images/css.png");
+		createProduct("1.6021766×10^−19", "Edge Tech HTML5", "/images/html.png");
+		createProduct("137.036", "Edge Tech Java 1.8", "/images/java.png");
+		createProduct("9.46073047 x 10^12", "Edge Tech JavaScript", "/images/javascript.png");
+		createProduct("2.718281828459045", "Edge Tech MongoDB", "/images/mongodb.png");
+		createProduct("299792458", "Edge Tech MySQL", "/images/mysql.png");
+		createProduct("13.799±0.021*10^9", "Edge Tech Spring Boot", "/images/spring.png");
+		createProduct("384400", "Edge Tech SQL", "/images/sql.png");
 
-		createCustomer("Edge Tech Academy", "8701 Bedford-Euless Rd", 	"682-334-5679");
-		createCustomer("First Command", 	"1 First Command Rd", 		"682-334-5679");
-		createCustomer("Acora Education", 	"8701 Bedford-Euless Rd", 	"682-334-5679");
-		createCustomer("American Airlines", "1 First Command Rd", 		"682-334-5679");
-		createCustomer("Homebid.com", 		"Scottsdale AZ", 			"801-225-2030");
 	}
 
-	public void createProduct(String productId, String description, String imageUrl ) {
+	public void createProduct(String productId, String description, String imageUrl) {
 		Product product;
 
-		product = new Product(productId, description, imageUrl, new BigDecimal( Math.random()*50 ));
+		product = new Product(productId, description, imageUrl, new BigDecimal(Math.random() * 50));
 		productService.add(product);
 		log.info(product);
 	}
-
-	public void createCustomer(String name, String address, String phoneNum ) {
-		Customer customer;
-
-		customer = new Customer(name, address, phoneNum);
-		customerService.saveCustomer(customer);
-		log.info(customer);
-	}}
+}
